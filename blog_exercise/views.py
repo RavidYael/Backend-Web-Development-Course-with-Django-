fce7b5ed22151030ba292d55284823705831f6c6
Ravid Yael <ravid.yael.7@gmail.com>
2022-07-16 14:49:40 +0300

    Adding Template Usage

--- blog_exercise/views.py
+++ blog_exercise/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def welcome_blog(request):
-    return HttpResponse('<h1>Welcome To My Blog</h1>')
+    return render(request, 'blog_home.html')
