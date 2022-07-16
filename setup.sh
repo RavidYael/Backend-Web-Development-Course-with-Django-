b2da042040cfc686054364f9851503976139ca00
Ravid Yael <ravid.yael.7@gmail.com>
2022-07-16 12:32:51 +0300

    Setting up the VM & Django + Automating script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Install Pipenv, the -n option makes sudo fail instead of asking for a
+# password if we dont have sufficient privileges to run it
+sudo -n dnf install -y pipenv
+
+cd /vagrant
+# Install dependencies with Pipenv
+pipenv sync --dev
+
+# run database migrations
+pipenv run python manage.py migrate
+
+# run our app on localhost port
+setsid pipenv run python manage.py runserver 0.0.0.0:8000 > runserver.log 2>&1 &
